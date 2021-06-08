a0b450449bc0f8f8b893bd2656064e56ea781e8f
Gael Porteu de La Morandière <gael.porteu@gmail.com>
2021-06-08 16:12:43 -0400

    Add reverseString

--- Beginner/reverseString/index-START.js
+++ Beginner/reverseString/index-START.js
@@ -6,7 +6,36 @@ E.g reverseString('algorithms') // should return 'smhtirogla'
 
 
 function reverseString(text) {
-    // Code goes here
+    // Test 1
+    // return text.split("").reverse().join("");
+    
+    // Test 2 spread operator
+    // return [...text].reverse().join('');
+    
+    // Test 3 for loop
+    // let result = "";
+    // for (let i = text.length -1; i >= 0; i--) {
+    //     result += text[i];
+    // }
+    // return result;
+
+    // Test 4 for...of
+    // let result = "";
+    // for (let char of text) {
+    //     result = char + result;
+    // }
+    // return result;
+
+    // Test 5 using if else and .substr
+    // if (text === "") {
+    //     return "";
+    // } else {
+    //     return reverseString(text.substr(1)) + text[0];
+    // }
+
+    // Test 6 using the reduce() method
+    return text.split('').reduce((acc, char) => char + acc, "");
+
 }
 
 
